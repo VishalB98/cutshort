501effc3aaa979ea52d4e4fecb56aae74c6e62db
Vishal Bisht <V.BISHT9811@GMAIL.COM>
2022-10-07 14:39:15 +0530

    Deployed

--- src/features/onboarding/OnboardingFeature.js
+++ src/features/onboarding/OnboardingFeature.js
@@ -1,27 +1,24 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { NUMBEROFONBOARDINGSTEP } from '../../data/constants';
 import { connect } from 'react-redux'
 
-const HeaderComponent = React.lazy(() => import('./components/HeaderComponent'));
-const StepComponent = React.lazy(() => import('./components/StepComponent'));
-const Heading = React.lazy(() => import('./components/Heading'));
-const InputComponent = React.lazy(() => import('./components/InputComponent'));
-const CardComponent = React.lazy(() => import('./components/CardComponent'));
-const ButtonComponent = React.lazy(() => import('./components/ButtonComponent'));
-
+import ButtonComponent from './components/ButtonComponent';
+import HeaderComponent from './components/HeaderComponent'
+import Heading from './components/Heading';
+import InputComponent from './components/InputComponent';
+import StepComponent from './components/StepComponent'
+import CardComponent from './components/CardComponent';
 
 function OnboardingFeature(props) {
 
   return (
 
     <div className='flex-1'>
-      <Suspense fallback={<div>Loading...</div>}>
         <HeaderComponent />
         <StepComponent />
         <Heading />
         {props.screenNumber < NUMBEROFONBOARDINGSTEP ? props.screenNumber === 3 ? <CardComponent /> : <InputComponent /> : null}
         <ButtonComponent />
-      </Suspense>
     </div>
 
   )
